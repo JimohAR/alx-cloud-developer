7d423f48b932810babf703bee7ef4a228d0f8c1c
JimohAR <ec2-user@ip-172-31-82-162.ec2.internal>
2022-09-06 22:54:01 +0000

    Update todoDataLayer to implement AWS XRay tracing

--- course-04/project/c4-final-project-starter-code/backend/src/dataLayer/todoDataLayer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/dataLayer/todoDataLayer.ts
@@ -1,4 +1,5 @@
 import * as AWS from "aws-sdk";
+import * as AWSXRay from "aws-xray-sdk"
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { Types } from 'aws-sdk/clients/s3';
 import { TodoItem } from "../models/TodoItem";
@@ -6,11 +7,12 @@ import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 
 const logger = createLogger('dataLayer')
+const XAWS = AWSXRay.captureAWS(AWS)
 
 export class DataLayer {
     constructor(
-        private readonly dbClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
-        private readonly s3Client: Types = new AWS.S3({ signatureVersion: 'v4' }),
+        private readonly dbClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
+        private readonly s3Client: Types = new XAWS.S3({ signatureVersion: 'v4' }),
         private readonly s3BucketName = process.env.ATTACHMENT_S3_BUCKET,
         private readonly todosTable = process.env.TODOS_TABLE,
         private readonly urlExp = process.env.SIGNED_URL_EXPIRATION
